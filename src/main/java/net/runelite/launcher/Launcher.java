addfbcc670f570841d657315a380c1006f52d638
BoomScape <james@boom-scape.com>
2025-06-11 16:10:17 -0600

    Use tab here

--- src/main/java/net/runelite/launcher/Launcher.java
+++ src/main/java/net/runelite/launcher/Launcher.java
@@ -89,7 +89,7 @@ import org.slf4j.LoggerFactory;
 public class Launcher
 {
 	static final File RUNELITE_DIR = new File(System.getProperty("user.home"), ".boomps");
-    static final File LOGS_DIR = new File(RUNELITE_DIR, "logs");
+	static final File LOGS_DIR = new File(RUNELITE_DIR, "logs");
 	static final File REPO_DIR = new File(RUNELITE_DIR, "repository2");
 	public static final File CRASH_FILES = new File(LOGS_DIR, "jvm_crash_pid_%p.log");
 	private static final String USER_AGENT = "BoomPS/" + LauncherProperties.getVersion();
